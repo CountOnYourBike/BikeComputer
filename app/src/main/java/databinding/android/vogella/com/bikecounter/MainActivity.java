b806c64a3cbc7c3be2f9a97bfbecab227c728f1e
AleksanderKondrat <aleksander.kondrat@gmail.com>
2018-11-14 13:45:25 +0100

    First commit

---
+++ app/src/main/java/databinding/android/vogella/com/bikecounter/MainActivity.java
@@ -0,0 +1,64 @@
+package databinding.android.vogella.com.bikecounter;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mActionBarDrawerToggle;
+    private NavigationView mNavigationView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initViewObject();
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        return mActionBarDrawerToggle.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
+    }
+
+    private void initViewObject(){
+        mDrawerLayout=(DrawerLayout)findViewById(R.id.activity_main);
+        mActionBarDrawerToggle = new ActionBarDrawerToggle(this,mDrawerLayout,R.string.open,R.string.close);
+
+        mDrawerLayout.addDrawerListener(mActionBarDrawerToggle);
+        mActionBarDrawerToggle.syncState();
+//        mActionBarDrawerToggle.setDrawerIndicatorEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mNavigationView = (NavigationView)findViewById(R.id.navigation_view);
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                int id = menuItem.getItemId();
+
+                switch (id) {
+                    case R.id.profile:
+                        Toast.makeText(MainActivity.this, R.string.profile, Toast.LENGTH_SHORT).show();
+                    case R.id.settings:
+                        Toast.makeText(MainActivity.this, R.string.settings, Toast.LENGTH_SHORT).show();
+                    case R.id.configuration:
+                        Toast.makeText(MainActivity.this, R.string.configuration, Toast.LENGTH_SHORT).show();
+                    case R.id.exit:
+                        Toast.makeText(MainActivity.this, R.string.exit, Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            }
+        });
+
+    }
+}
